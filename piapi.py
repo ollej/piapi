54c4fa715a4001c419be02d2dc97c9ee13510fe8
Olle Johansson <Olle@Johansson.com>
2017-04-28 00:15:00 +0200

    Extract queue job db into piqueue. Remove·flask-sqlalchemy dependency.

--- piapi.py
+++ piapi.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
-from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-from sqlalchemy.types import TypeDecorator
 from flask_marshmallow import Marshmallow
-import datetime
-import json
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+from piqueue import piqueue
+
+# Setup database: Base.metadata.create_all(engine)
 
 # TODO: Return JSON on failure
 # TODO: Refactor brightness/rotate params
@@ -15,43 +16,16 @@ import json
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/queue.db'
-db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
-class TextPickleType(TypeDecorator):
-    SIZE = 4096
-    impl = sqlalchemy.Text(SIZE)
-
-    def process_bind_param(self, value, dialect):
-        if value is not None:
-            value = json.dumps(value)
-        return value
-
-    def process_result_value(self, value, dialect):
-        if value is not None:
-            value = json.loads(value)
-        return value
-
-class Job(db.Model):
-    __tablename__ = 'queue'
-    id = db.Column(db.Integer, primary_key=True)
-    job_name = db.Column(db.String(80))
-    options = db.Column(TextPickleType())
-    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
-
-    def __init__(self, job_name, options):
-        self.job_name = job_name
-        self.options = options
-
-    def __repr__(self):
-        return '<Job %r>' % self.job_name
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class JobSchema(ma.Schema):
     class Meta:
         # Fields to expose
         fields = ('id', 'job_name', 'date_created', 'options')
 
-job_schema = JobSchema()
 job_schemas = JobSchema(many=True)
 
 def clean_dict(data):
@@ -69,15 +43,15 @@ class Queue(Resource):
         parser.add_argument('interval', type=float, help='Seconds to sleep between each iteration')
         args = parser.parse_args()
 
-        job = Job(job_name, clean_dict(args))
-        db.session.add(job)
-        db.session.commit()
+        job = piqueue.Job(job_name, clean_dict(args))
+        session.add(job)
+        session.commit()
 
         return '', 200
 
 class QueueList(Resource):
     def get(self):
-        queue = Job.query.all()
+        queue = session.query(piqueue.Job).order_by(piqueue.Job.id)
         return job_schemas.jsonify(queue)
 
     def delete(self):
---
+++ piqueue/piqueue.py
@@ -0,0 +1,37 @@
+import json
+import datetime
+import sqlalchemy
+from sqlalchemy.types import TypeDecorator
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class TextPickleType(TypeDecorator):
+    SIZE = 4096
+    impl = Text(SIZE)
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+class Job(Base):
+    __tablename__ = 'queue'
+    id = Column(Integer, primary_key=True)
+    job_name = Column(String(80))
+    options = Column(TextPickleType())
+    date_created = Column(DateTime, default=datetime.datetime.now)
+
+    def __init__(self, job_name, options):
+        self.job_name = job_name
+        self.options = options
+
+    def __repr__(self):
+        return "<Job(id='%r', job_name='%r'>" % (self.id, self.job_name)
+
