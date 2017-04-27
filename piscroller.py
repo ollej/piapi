f12113fe5948aa547a5488f0aa211131bffd98c3
Olle Johansson <Olle@Johansson.com>
2017-04-27 21:54:11 +0200

    Split ScrollJob class into separate file.

--- piscroller.py
+++ piscroller.py
@@ -24,46 +24,10 @@ Options:
 # TODO: Setup setuptools
 
 import sys
-import time
 import signal
 
-import scrollphat
 from docopt import docopt
-
-class ScrollJob:
-    def __init__(self, options):
-        self.options = options
-
-    def init(self):
-        scrollphat.set_brightness(self.options['brightness'])
-        scrollphat.set_rotate(self.options['rotate'])
-
-        message = self.options['message'] + '    '
-        if self.options['upper'] == True:
-            message = message.upper()
-        scrollphat.write_string(message, 11)
-
-    def run(self):
-        self.init()
-        length = scrollphat.buffer_len()
-        if self.options['loop'] == True:
-            counter = 0
-            while True:
-                self.scroll()
-                counter += 1
-                if counter % length == 0:
-                    time.sleep(self.options['sleep'])
-        else:
-            for i in range(length):
-                self.scroll()
-            time.sleep(self.options['sleep'])
-
-    def scroll(self):
-        scrollphat.scroll()
-        time.sleep(self.options['interval'])
-
-    def cleanup(self):
-        scrollphat.clear()
+from pijobs.ScrollJob import ScrollJob
 
 class JobCommand:
     def __init__(self, job, opts):
