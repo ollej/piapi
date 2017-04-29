9e670f639b8ca2108fc43f409392d310b7964af6
Olle Johansson <Olle@Johansson.com>
2017-04-29 12:09:40 +0200

    Decode fortune output from byte array to string.

--- pijobs/fortunejob.py
+++ pijobs/fortunejob.py
@@ -3,9 +3,16 @@ from pijobs.scrolljob import ScrollJob
 
 class FortuneJob(ScrollJob):
     def message(self):
-        return self.run_cmd('fortune')
+        return self.run_cmd(['fortune', '-s'])
 
     def run_cmd(self, cmd):
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        output = p.communicate()[0]
+        output = self.decode_output(p.communicate()[0])
+        return output
+
+    def decode_output(self, output):
+        if output is not None:
+            output = output.decode("utf-8").rstrip()
+        else:
+            output = ''
         return output
