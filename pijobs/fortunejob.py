02d90c515f4616092a15f3871db21ed306e12a6b
Olle Johansson <Olle@Johansson.com>
2017-04-29 11:55:45 +0200

    Fixed run_cmd bug.

--- pijobs/fortunejob.py
+++ pijobs/fortunejob.py
@@ -5,7 +5,7 @@ class FortuneJob(ScrollJob):
     def message(self):
         return self.run_cmd('fortune')
 
-    def run_cmd(cmd):
+    def run_cmd(self, cmd):
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = p.communicate()[0]
         return output
