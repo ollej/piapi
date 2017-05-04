97b9162f382d72dea3f777a2215300cd2694f3f7
Olle Johansson <Olle@Johansson.com>
2017-05-04 23:20:44 +0200

    Add sparkles demo job.

---
+++ pijobs/sparklesjob.py
@@ -0,0 +1,20 @@
+import math
+import unicornhat as unicorn
+from random import randint
+from pijobs.unicornhatjob import UnicornhatJob
+
+class SparklesJob(UnicornhatJob):
+    def run(self):
+        for loop in range(int(self.options['loop']) * self.rows * self.cols):
+            x = randint(0, self.cols - 1)
+            y = randint(0, self.rows - 1)
+            r, g, b = self.calculate_color(x, y)
+            unicorn.set_pixel(x, y, r, g, b)
+            unicorn.show()
+            self.sleep_interval()
+
+    def calculate_color(self, x, y):
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        return [r, g, b]
